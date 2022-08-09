6f6f6b4d174be1ed61a4c79dd497afc7e9b65431
PoyrazM <mertcan_1999@icloud.com>
2022-08-09 23:22:17 +0300

    saturdayWork is completed

--- saturday/src/main/java/StreamForFilterMap.java
+++ saturday/src/main/java/StreamForFilterMap.java
@@ -1,6 +1,7 @@
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /*
@@ -14,17 +15,16 @@ public class StreamForFilterMap {
     }
 
     @Test(priority = 2)
-    public void streamMap(){
-
+    public void streamMapForEach(){
+        List<String> name = Arrays.asList("Patika.dev", "Enuygun");
+        name.stream().map(s ->   s.contains("Patika.dev")).forEach(System.out::println);
+        name.stream().map(s -> s.contains("Enuygun")).forEach(System.out::println);
     }
 
     @Test(priority = 3)
     public void streamFilter(){
-
-    }
-
-    @Test(priority = 4)
-    public void streamForEach(){
-
+        List<String> name = Arrays.asList("Patika.dev", "Enuygun", "Kodluyoruz");
+        List result = name.stream().filter(s -> s.startsWith("K")).collect(Collectors.toList());
+        System.out.println(result);
     }
 }
